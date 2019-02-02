d4e8bfc45af9b0301d2d07dc2789571aea23c479
Mike Shapiro <michaelshapiro98@gmail.com>
2019-02-01 18:01:32 -0800

    refine

--- JSON and AJAX/main.js
+++ JSON and AJAX/main.js
@@ -5,5 +5,8 @@ var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-1.json');
 
 ourRequest.onload = function(){
-    console.log(ourRequest.responseText); 
+    //console.log(ourRequest.responseText); 
+    var ourData = JSON.Pa   //ourRequest.responseText;
+    console.log(ourData[0]);
 };
+ourRequest.send();
