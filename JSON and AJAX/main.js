4130f75690613bde3fa6e9bca0dcc272bc023f01
Mike Shapiro <michaelshapiro98@gmail.com>
2019-02-02 18:14:58 -0800

    Change to an onclick event.

--- JSON and AJAX/main.js
+++ JSON and AJAX/main.js
@@ -1,12 +1,13 @@
+var btn = document.getElementById("btn");
+
 
 // XMLHttpRequest allows the browser to connect to the 
-
 var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-1.json');
 
 ourRequest.onload = function(){
     //console.log(ourRequest.responseText); 
-    var ourData = JSON.Pa   //ourRequest.responseText;
+    var ourData = JSON.parse(ourRequest.responseText)   //ourRequest.responseText;
     console.log(ourData[0]);
 };
 ourRequest.send();
