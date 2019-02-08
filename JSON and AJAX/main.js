700eb5c176e82761634b6368e06d364606556c93
Mike Shapiro <michaelshapiro98@gmail.com>
2019-02-07 16:57:29 -0800

    Adding to project

--- JSON and AJAX/main.js
+++ JSON and AJAX/main.js
@@ -2,4 +2,4 @@
 // XMLHttpRequest allows the browser to connect to the 
 
 var ourRequest = new XMLHttpRequest();
-
+ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-1.json'); 
